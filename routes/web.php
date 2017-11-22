7bdbeb6c8d52f37ee6945dd4e406d9f7b76ea86c
i65 <2461585309@qq.com>
2017-11-22 15:49:35 +0800

    Finish login and logou

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesControler@about')->name('about');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
@@ -26,4 +26,8 @@ Route::post('/users', 'UsersController@store')->name('users.store');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
-*/
+ */
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
